42111728fa835b78626ce170836c19fdfb3743d8
yuri2bdev <yuribritoeng@gmail.com>
2022-09-13 23:20:20 -0300

    inicia o projeto

---
+++ src/main/java/com/parents/paisfilhos/PaisFilhosApplication.java
@@ -0,0 +1,13 @@
+package com.parents.paisfilhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaisFilhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaisFilhosApplication.class, args);
+	}
+
+}
