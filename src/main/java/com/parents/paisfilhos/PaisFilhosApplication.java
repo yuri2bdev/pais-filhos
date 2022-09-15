e9fc563a60f974a60073af9e7617f7895daa11ab
yuri2bdev <yuribritoeng@gmail.com>
2022-09-14 22:51:30 -0300

    implementa API de teste

--- src/main/java/com/parents/paisfilhos/PaisFilhosApplication.java
+++ src/main/java/com/parents/paisfilhos/PaisFilhosApplication.java
@@ -2,9 +2,19 @@ package com.parents.paisfilhos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class PaisFilhosApplication {
+	
+	@GetMapping
+	public String getHomeTeste() {
+		return "Pais e Filhos - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaisFilhosApplication.class, args);
